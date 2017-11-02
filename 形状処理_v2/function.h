dc9ab0c9976b0c29f389d3a967ef0bee85b54618
kawakami <karutaswitch418@gmail.com>
2017-11-03 08:38:07 +0900

    opengl関数のオーバーロードを追加
    定義はfunction.h
    実装はglOverload.cppでやって

--- 形状処理_v2/function.h
+++ 形状処理_v2/function.h
@@ -6,7 +6,11 @@
 
 // CalcUtil.cpp
     GLOBAL double CalcBsplineFunc(int i, int M, double t, double* knot);
+    GLOBAL double Calc1DiffBsplineFunc(int i, int M, double t, double* knot);
     GLOBAL void CalcBsplineFunc2(double t, int mord, int ncpnt, int nknot, double* knot, int reqord, double* bf);
 
 // DrawUtil.cpp
     GLOBAL void DrawBsplineFunc(int mord, int ncpnt, int nknot, double* knot, double min_t = 0.0, double max_t = 1.0);
+
+// glOverload.cpp
+    GLOBAL void glVertex3d(const Vector3d& vec);
---
+++ 形状処理_v2/glOverload.cpp
@@ -0,0 +1,3 @@
+#include "GV.h"
+
+void glVertex3d(const Vector3d& vec) { glVertex3d(vec.X, vec.Y, vec.Z); }
