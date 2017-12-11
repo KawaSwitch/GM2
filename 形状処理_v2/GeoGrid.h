0e01aa6df5e9207ed742e27f557ce8f9fc46175f
kawakami <karutaswitch418@gmail.com>
2017-12-11 21:45:18 +0900

    2Dグリッドデプス値無効
    またなんかおかしかったら変える
    大きさとか指定できないのもおかしいので

--- 形状処理_v2/GeoGrid.h
+++ 形状処理_v2/GeoGrid.h
@@ -11,26 +11,30 @@ struct GeoGrid2D
 {
     void Draw() const
     {
+        glDisable(GL_DEPTH_TEST);
+
         // 色：グレー
         glColor4d(0.8, 0.8, 0.8, 1.0);
 
-        for (double x = -50; x <= 50; x += 1)
+        for (double x = -200; x <= 200; x += 5)
         {
-            for (double y = -50; y <= 50; y += 1)
+            for (double y = -200; y <= 200; y += 5)
             {
                 glBegin(GL_LINES);
 
                 // x-direction
-                glVertex3d(-50, y, 0);
-                glVertex3d(50, y, 0);
+                glVertex3d(-200, y, 0);
+                glVertex3d(200, y, 0);
 
                 // y-direction
-                glVertex3d(x, -50, 0);
-                glVertex3d(x, 50, 0);
+                glVertex3d(x, -200, 0);
+                glVertex3d(x, 200, 0);
 
                 glEnd();
             }
         }
+
+        glEnable(GL_DEPTH_TEST);
     }
 };
 
